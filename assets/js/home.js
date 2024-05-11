d4e1510de4958e44c98056dbabed10d69ed5e24b
Manjari Jain <manjjain@paypal.com>
2024-05-11 19:37:25 +0530

    added create game on ui

--- assets/js/home.js
+++ assets/js/home.js
@@ -6,7 +6,7 @@ function fetchdata() {
     type: "get",
     success: function (data) {
       console.log(JSON.stringify(data));
-      const playerGameMappings = data.playerGameMapping;
+      const playerGameMappings = data.data.playerGameMapping;
       if (playerGameMappings) {
         $("#top-5-players li").remove();
         for (let player of playerGameMappings) {
@@ -22,8 +22,8 @@ function fetchdata() {
           </li>`
           );
         }
+        console.log("completed rendering");
       }
-      console.log("completed rendering");
     },
     complete: function (data) {
       setTimeout(fetchdata, 20000);
@@ -32,6 +32,36 @@ function fetchdata() {
     console.log("access denied ", err);
   });
 }
+
 $(document).ready(function () {
   setTimeout(fetchdata, 20000);
+});
+
+const createGameButton = $("#create-game");
+const gameDisplay = $("#game-display");
+
+createGameButton.on("click", function (e) {
+  e.preventDefault();
+  console.log("button clicked");
+  $.ajax({
+    url: "/games/",
+    type: "post",
+    success: function (data) {
+      console.log(JSON.stringify(data));
+      const playerScores = data.responseData.playerScores;
+      let string = `<div style="border:1px solid;margin:10px;width:500px">`;
+      if (playerScores) {
+        for (let player of playerScores) {
+          string += `   <li style="margin:20px;width:300px">
+                  <b>${player.name}</b>
+                  <b>Score </b> ${player.score}
+                </li>`;
+        }
+        string.append += `</div>`;
+        gameDisplay.append(string);
+      }
+    },
+  }).fail(function (err) {
+    console.log("access denied ", err);
+  });
 });
